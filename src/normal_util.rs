c3ea7716c1534b3d04b4b0a813c64c0098801aec
Jonathan Bredin <bredin@acm.org>
2020-10-06 15:08:18 -0500

    Adding notate to normal-util

--- src/normal_util.rs
+++ src/normal_util.rs
@@ -1,4 +1,5 @@
 use normal::Normal;
+use regex::Regex;
 use std::path::PathBuf;
 use std::process::exit;
 use structopt::StructOpt;
@@ -17,6 +18,9 @@ struct Opt {
 
     #[structopt(short, long)]
     insert: Option<String>,
+
+    #[structopt(short, long)]
+    note: Option<String>,
 
     #[structopt(short, long)]
     search: Option<String>,
@@ -53,6 +57,18 @@ pub fn main() {
             },
         });
 
+    // notate id, column, text
+    opt.note.map(|id_col_text_str| parse_notate(id_col_text_str)).
+        map(|id_col_text_str| match id_col_text_str {
+            (id, col, note) => match normal.notate(id, col.as_ref(), note.as_ref()) {
+                Ok(()) => (),
+                Err(err) => {
+                    println!("error: {}", err.msg);
+                    exit(1);
+                }
+            }
+        });
+
     // substring search
     opt.search.map(|search| normal.search(search.as_str())).
         map(|search_result| match search_result {
@@ -72,4 +88,25 @@ pub fn main() {
                 exit(1);
             },
         });
+}
+
+/// Break up the command-line argument to notate an entry as "id column lots of note text following...."
+fn parse_notate(id_col_text_str: String) -> (i64, String, String) {
+    let re = Regex::new(r"(\d+)\s+([^\s]+)\s+(.*)").unwrap();
+    let tokens = re.captures_iter(id_col_text_str.as_str()).next().unwrap();
+    (tokens.get(1).unwrap().as_str().parse::<i64>().unwrap(),
+        tokens.get(2).unwrap().as_str().to_string(),
+        tokens.get(3).unwrap().as_str().to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_notate() {
+        assert_eq!(
+            parse_notate("1 note nota bene".to_string()),
+            (1, "note".to_string(), "nota bene".to_string()));
+    }
 }
